9803ab2551f0f4fee992fd82eb4007bdf71b6290
sanjay-kumar-singh-73 <145865352+sanjay-kumar-singh-73@users.noreply.github.com>
2025-04-19 00:53:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const userName = document.getElementById("lebel");
+const pswd  = document.getElementById("lebel2");
+
+userName.addEventListener("click", ()=> {
+    // this.style.position = "relative";
+})
